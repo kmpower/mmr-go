78dd7cc00ca60f5c8378b4dc9011419b403bd3ae
KM <info@kmpowerllc.com>
2022-07-16 02:28:11 -0700

    test code

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hi")
+	record := Record{}
+	fmt.Println(record)
+}
+
+type Patient struct {
+	FirstName string
+	LastName  string
+}
+
+type Docter struct {
+	FirstName string
+	LastName  string
+}
+
+type Medicine struct {
+	MedicineName     string
+	MedicineQuantity string
+}
+
+type Record struct {
+	Patient
+	Docter
+	Medicine
 }
